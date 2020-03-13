c38a846935d035c5657248ecac2a176ccaadd91c
Nepcam <cameron.nepe@gmail.com>
2020-03-13 22:28:35 +1300

    Add project files.

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare your variables in one place
+            int serialNo = 100;
+            string accountNo = "";
+            float anyNo = 4.2f;
+
+            // accountNo = serialNo; *** cannot convert 'int' to 'string' (use a conversion method see example below) ***
+            accountNo = serialNo.ToString();
+            Console.WriteLine(accountNo);
+
+            // serialNo = anyNo; *** cannot convert 'float' to 'int'
+            serialNo = Convert.ToInt32(anyNo);
+            Console.WriteLine(serialNo);
+        }
+    }
+}
